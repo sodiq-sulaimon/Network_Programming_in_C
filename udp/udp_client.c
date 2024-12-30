1802c3d1de275863d5f2f258fcb89b5b593215f8
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-12-30 11:01:41 -0600

    Moved the headers to a header file.

--- udp/udp_client.c
+++ udp/udp_client.c
@@ -1,17 +1,4 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
-
-#define ISVALIDSOCKET(s) ((s) >= 0)
-#define CLOSESOCKET(s) close(s)
-#define SOCKET int
-#define GETSOCKETERRNO() (errno)
+#include "headers.h"
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
